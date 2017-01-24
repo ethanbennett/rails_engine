0e211e028a1bd58e4820f5ab40da842dbc4c6e8c
Ethan Bennett <ebennett19@gmail.com>
2017-01-24 15:10:03 -0700

    Seed everything but transactions

--- db/migrate/20170124054459_create_transactions.rb
+++ db/migrate/20170124054459_create_transactions.rb
@@ -1,8 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[5.0]
   def change
     create_table :transactions do |t|
-      t.integer  :credit_card_number, :limit => nil
+      t.integer :credit_card_number, :limit => 8
       t.string   :result
+      t.references :invoice, foreign_key: true
 
       t.timestamps
     end
