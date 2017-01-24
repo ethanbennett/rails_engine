88f8c1eef79962c9587bbf2421c079c86d02e842
Ethan Bennett <ebennett19@gmail.com>
2017-01-23 23:07:19 -0700

    Add models and tables for items and invoice items

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124054458) do
+ActiveRecord::Schema.define(version: 20170124060014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "invoice_id"
+    t.integer  "quantity"
+    t.integer  "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id", using: :btree
+    t.index ["item_id"], name: "index_invoice_items_on_item_id", using: :btree
+  end
+
   create_table "invoices", force: :cascade do |t|
-    t.integer  "customer_id"
+    t.integer  "customer"
     t.integer  "merchant_id"
     t.text     "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id", using: :btree
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.text     "name"
+    t.text     "description"
+    t.integer  "unit_price"
+    t.integer  "merchant_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["merchant_id"], name: "index_items_on_merchant_id", using: :btree
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -29,4 +51,8 @@ ActiveRecord::Schema.define(version: 20170124054458) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
+  add_foreign_key "invoices", "merchants"
+  add_foreign_key "items", "merchants"
 end
