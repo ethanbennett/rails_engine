bd5e043980c810ebd24a445bbe7084b256a4f5fe
Ethan Bennett <ebennett19@gmail.com>
2017-01-23 22:56:06 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: [:index]
       resources :invoices, only: [:index]
+      resources :items, only: [:index]
+      resources :invoice_items, only: [:index]
     end
   end
 end
