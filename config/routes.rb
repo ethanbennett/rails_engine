270986fbf3a44691bf6bd00b89e1e20eb89e1351
Ethan Bennett <ebennett19@gmail.com>
2017-01-24 19:34:31 -0700

    Add find_all routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,35 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :transactions do
         get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
       end
       resources :transactions,  only: [:index, :show]
 
       namespace :merchants do
         get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
       end
       resources :merchants,     only: [:index, :show]
 
       namespace :items do
         get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
       end
       resources :items,         only: [:index, :show]
 
+      namespace :invoices do
+        get "/find_all", to: "search#index"
+      end
       resources :invoices,      only: [:index]
+
+      namespace :invoice_items do
+        get "/find_all", to: "search#index"
+      end
       resources :invoice_items, only: [:index]
+
+      namespace :customers do
+        get "/find_all", to: "search#index"
+      end
       resources :customers,     only: [:index, :show]
     end
   end
