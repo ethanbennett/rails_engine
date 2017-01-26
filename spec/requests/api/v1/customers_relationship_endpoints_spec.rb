2d6a77ac784c3037279cf1a36abfda0838d5a750
Jason Conrad <jdconrad@liberty.edu>
2017-01-25 20:19:19 -0700

    add test for favorite merchant

--- spec/requests/api/v1/customers_relationship_endpoints_spec.rb
+++ spec/requests/api/v1/customers_relationship_endpoints_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 describe "invoice rlationship endpoints" do
 
-
   context "get the invoices associated with a customer" do
     it "returns the invoices associated with a customer" do
       create_list(:merchant, 3)
       create(:customer)
-      create_list(:invoice, 5, merchant_id: Merchant.first.id, customer_id: Customer.first.id)
+      create_list(:invoice, 4, merchant_id: Merchant.first.id, customer_id: Customer.first.id)
 
       get "/api/v1/customers/#{Customer.first.id}/invoices"
 
@@ -24,13 +23,30 @@ describe "invoice rlationship endpoints" do
       create(:customer)
       create_list(:invoice, 3, merchant_id: Merchant.first.id, customer_id: Customer.first.id)
       create_list(:transaction, 5, invoice_id: Invoice.first.id)
-      
+
+
       get "/api/v1/customers/#{Customer.first.id}/transactions"
 
       transactions = JSON.parse(response.body)
 
       expect(response).to be_success
       expect(transactions.count).to eq(5)
+    end
+  end
+
+  context "get a customer's favorite merchant" do
+    it "returns a customer's favorite merchant" do
+      create_list(:merchant, 3)
+      create_list(:customer, 2)
+      create_list(:invoice, 2, customer_id: Customer.first.id, merchant_id: Merchant.first.id)
+      create_list(:transaction, 3, invoice_id: Invoice.first.id)
+      create(:invoice, customer_id: Customer.first.id, merchant_id: Merchant.last.id)
+
+      get "/api/v1/customers/#{Customer.first.id}/favorite_merchant"
+
+      favorite_merchant = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(favorite_merchant["id"]).to eq(Merchant.first["id"])
     end
   end
 end
