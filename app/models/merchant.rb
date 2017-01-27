813be542c8d90b95e0675f5c9c2091e609fcb190
Ethan Bennett <ebennett19@gmail.com>
2017-01-26 18:23:12 -0700

    Refactor

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,21 +2,20 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
 
-  def revenue(date=nil)
-    if date
-      self.invoices.where(created_at: date)
-      .joins(:transactions, :invoice_items)
-      .where(transactions: {result: "success"})
-      .sum('invoice_items.unit_price * invoice_items.quantity')
-    else
-      self.invoices.joins(:transactions, :invoice_items)
-      .where(transactions: {result: "success"})
-      .sum('invoice_items.unit_price * invoice_items.quantity')
-    end
+  scope :date?, lambda { |date|
+    return nil unless date.present?
+    where(invoices: {created_at: date})
+  }
+
+  def get_revenue(date=nil)
+    self.invoices.merge(Merchant.date?(date))
+    .joins(:transactions, :invoice_items)
+    .where(transactions: {result: "success"})
+    .sum('invoice_items.unit_price * invoice_items.quantity')
   end
 
   def self.total_revenue(date)
-    totals = Merchant.all.map { |merchant| merchant.revenue(date) }
+    totals = Merchant.all.map { |merchant| merchant.get_revenue(date) }
     totals.reduce(:+)
   end
 end
