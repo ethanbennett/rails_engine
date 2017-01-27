593cff55e51d49a9c6d2cc70a6cbff0b3a27a413
Ethan Bennett <ebennett19@gmail.com>
2017-01-26 21:53:44 -0700

    Remove pry

--- app/controllers/api/v1/merchants/random_controller.rb
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::Merchants::RandomController < ApplicationController
 
   def show
     random_id = rand(Merchant.count)
-    # binding.pry
     random_merchant = Merchant.find(random_id)
     render :json => random_merchant, :serializer => MerchantSerializer
   end
